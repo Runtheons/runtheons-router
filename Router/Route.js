59350c9123474a01a5c677419e8bc7bfcdce847b
Zexal0807 <gallinar00@gmail.com>
2020-12-29 21:17:40 +0100

    Fix - Route haven't the load method

--- Router/Route.js
+++ Router/Route.js
@@ -5,4 +5,12 @@ module.exports = class Route {
 
     method = "GET";
 
+
+    load(router) {
+        if (this.isAvaible()) {
+            console.log(this.path + " is loaded");
+            var method = this.method.toLowerCase();
+            router[method](this.path, this.resolve);
+        }
+    }
 }
