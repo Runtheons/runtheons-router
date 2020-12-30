db81adfc418e8562e8999d2c0573cb47f596c055
Zexal0807 <gallinar00@gmail.com>
2020-12-30 12:44:27 +0100

    Implement resolve  - fix require name  - implement resolve  - add getSession  - add getData  - add isAuthorized and isValid  - add handles

--- Route.js
+++ Route.js
@@ -1,5 +1,6 @@
+const validator = null; //require("@runtheons/validate");
 const responseFactory = require("@runtheons/response-factory");
-const Authorizzation = require("@runtheons-authorizzation");
+const Authorizzation = require("@runtheons/authorizzation");
 
 module.exports = class Route {
 
@@ -24,7 +25,60 @@ module.exports = class Route {
             router[method](this.path, this.resolve);
         }
     }
+
     resolve(req, res) {
+        //Get Data
+        var data = this.getData(req);
+        var session = this.getSession(req);
+        var headerResponseOption = this.getOptions(req);
+
+        var responseData = {}
+
+        //Authorizzation
+        var auth = this.isAuthorized(session);
+        if (auth.status) {
+            //Validation
+            var valid = this.isValid(data);
+            if (valid.status) {
+                responseData = this.functionHandle(data, session);
+            } else {
+                responseData = this.notValidDataHandle(valid.errors);
+            }
+        } else {
+            responseData = this.notAuthorizedHandle(auth.errors);
+        }
         //Make Response with headerResponseOption
         responseFactory.setReponse(res);
+        responseFactory.send(responseData, headerResponseOption);
+    }
+
+    getSession(req) {
+    }
+
+    getData(req) {
+    }
+
+    isAuthorized(session) {
+    }
+
+    notAuthorizedHandle = function(err) {
+        return {
+            msg: "Unauthorized user",
+            status: false
+        };
+    };
+    schema = {};
+
+    isValid(data) {
+        //Using Runtheons Validator
+        var valid = validator.validate(this.schema, data);
+        return valid;
+    }
+    functionHandle = function(data, session) {
+        return {
+            msg: "OK",
+            status: true
+        };
+    };
+
 }
