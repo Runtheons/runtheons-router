d64bd5811a82b701bc799228ce3c8ab143362b97
Zexal0807 <gallinar00@gmail.com>
2021-01-21 10:46:48 +0100

    Add prefix for start doc

--- doc.js
+++ doc.js
@@ -1,8 +1,13 @@
 var router = require("./Router");
 router = new router(null);
 
-var routes = router.getAvaibleRoute(process.argv[2]);
+const path = require("path");
+const routepath = path.join(process.cwd(), "./../../../", process.argv[2]);
+var routes = router.getAvaibleRoute(routepath);
+
+console.log("Start generate doc in " + routepath);
 
 const routeDocumentation = require("@runtheons/route-documentation");
 routeDocumentation.setConfig({ template: "1" })
 routeDocumentation.generateDoc(routes);
+console.log("Generated doc");
