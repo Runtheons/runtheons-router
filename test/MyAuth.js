e824840838c33d95e888dc0ca52b9b84f46126b6
Zexal0807 <gallinar00@gmail.com>
2022-03-20 10:39:08 +0100

    Add test for increase coverage

--- test/MyAuth.js
+++ test/MyAuth.js
@@ -3,20 +3,12 @@ const Authorizzation = require('@runtheons/authorizzation');
 Authorizzation.add('TEST', {
 	check: (session) => {
 		return session == undefined;
-	},
-	success: () => {},
-	error: () => {
-		return {};
 	}
 });
 
 Authorizzation.add('TEST_NOT_AUTHORIZE', {
 	check: (session) => {
 		return false;
-	},
-	success: () => {},
-	error: () => {
-		return {};
 	}
 });
 
